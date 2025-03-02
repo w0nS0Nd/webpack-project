9e4792c345d5def67d7a62de42721084d9095c4f
w0nS0Nd <dimzyrgamer@gmail.com>
2025-03-03 00:45:44 +0200

    Create webpackconfig.js

---
+++ webpackconfig.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, 
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/pages/index.html',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/about.html',
+            filename: 'about.html',
+        }),
+    ],
+    mode: 'development',
+};
